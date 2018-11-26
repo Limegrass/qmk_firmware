68b297d6532d9aa2cb4a187479777d50ff22fa0f
Limegrass <multipresident@gmail.com>
2018-11-25 22:28:28 -0600

    First personal laplace config
    Added initial laplace config, to be finetuned.

--- keyboards/keebio/laplace/config-base.h
+++ keyboards/keebio/laplace/config-base.h
@@ -33,8 +33,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 7
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS {D3, F4, D2, F5, D7, B4, D0, E6}
-#define MATRIX_COL_PINS {D1, B5, F7, B1, B3, B2, B6}
+#define MATRIX_ROW_PINS {D3, F4, D2, F5, D7, B4, C6, E6}
+#define MATRIX_COL_PINS {D1, F6, F7, B1, B3, B2, B6}
 
 /* WS2812 RGB LED */
 #define RGB_DI_PIN D4
