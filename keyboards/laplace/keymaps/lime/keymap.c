8c9bc1ead66fe28b925e7bdaf60dc4e469ed4903
Limegrass <multipresident@gmail.com>
2018-12-15 11:51:44 -0600

    Removed buggy Shifted keys + Function layer keys
    Now dedicated shifted or unshifted again

--- keyboards/laplace/keymaps/lime/keymap.c
+++ keyboards/laplace/keymaps/lime/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB           , KC_Q          , KC_W        , KC_E                , KC_R                 , KC_T          , KC_Y        , KC_U        , KC_I        , KC_O        , KC_P         , KC_BSLS         , KC_BSPC ,
             KC_LCTL          , KC_A          , KC_S        , KC_D                , KC_F                 , KC_G          , KC_H        , KC_J        , KC_K        , KC_L        , KC_SCLN      , RCTL_T(KC_QUOT) ,
             KC_LSFT          , KC_Z          , KC_X        , KC_C                , KC_V                 , KC_B          , KC_N        , KC_M        , KC_COMM     , KC_DOT      , KC_SLSH      , RSFT_T(KC_ENT)  ,
-            FN1              , OSL3          , KC_LGUI     , KC_LALT             , ESC_FN2              , SPACE_FN3     , TILDE_FN3    , TT(_FN4)    , BSLS_FN2        , PIPE_FN1)
+            FN1              , OSL3          , KC_LGUI     , KC_LALT             , ESC_FN2              , SPACE_FN3     , KC_TILDE    , TT(_FN4)    , KC_BSLS        , ENTER_FN2)
 
                              ,
     [_FN1] = LAYOUT(
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_EQL           , KC_1          , KC_2        , KC_3                , KC_4                 , KC_5          , KC_6        , KC_7        , KC_8        , KC_9        , KC_0         , KC_MINS         , KC_BSPC ,
             KC_LCTL          , LSFT(KC_1)    , LSFT(KC_2)  , LSFT(KC_3)          , LSFT(KC_4)           , LSFT(KC_5)    , LSFT(KC_6)  , LSFT(KC_7)  , LSFT(KC_8)  , LSFT(KC_9)  , LSFT(KC_0)   , KC_ENT          ,
             KC_LSFT          , LGUI(KC_1)    , LGUI(KC_2)  , LGUI(KC_3)          , LGUI(KC_4)           , LGUI(KC_5)    , LGUI(KC_6)  , LGUI(KC_7)  , LGUI(KC_8)  , LGUI(KC_9)  , LGUI(KC_0)   , KC_RSFT         ,
-            _______          , _______       , KC_LGUI     , KC_LALT             , _______              , _______       , _______     , _______     , _______     , _______
+            _______          , _______       , KC_LGUI     , KC_LALT             , _______              , KC_BSPC       , _______     , _______     , _______     , _______
             )
 
                              ,
