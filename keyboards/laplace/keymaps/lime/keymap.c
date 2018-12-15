3a9fbe0275e46c342ac42bcb0297bd3f652ab70b
Limegrass <multipresident@gmail.com>
2018-12-15 11:23:34 -0600

    Updated Keymaps
    Easier access to shifted keys

--- keyboards/laplace/keymaps/lime/keymap.c
+++ keyboards/laplace/keymaps/lime/keymap.c
@@ -27,6 +27,12 @@
 #define BS_FN1 LT(_FN1, KC_BSPC)
 #define BS_FN2 LT(_FN2, KC_BSPC)
 
+#define BSLS_FN2 LT(_FN2, KC_BSLS)
+
+#define PIPE_FN1 LT(_FN1, KC_PIPE)
+
+#define TILDE_FN3 LT(_FN3, KC_TILDE)
+
 /* , */
 /*         [] = LAYOUT( */
 /*         XXXXXXX, XXXXXXX, XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX , XXXXXXX , XXXXXXX    , XXXXXXX , XXXXXXX   , XXXXXXX , */
@@ -35,13 +41,14 @@
 /*         XXXXXXX, XXXXXXX, KC_LGUI , KC_LALT , XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX , XXXXXXX , XXXXXXX */
 /*         ) */
 
+//TODO: Take advantage of LM(layer, mod)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
             KC_TAB           , KC_Q          , KC_W        , KC_E                , KC_R                 , KC_T          , KC_Y        , KC_U        , KC_I        , KC_O        , KC_P         , KC_BSLS         , KC_BSPC ,
             KC_LCTL          , KC_A          , KC_S        , KC_D                , KC_F                 , KC_G          , KC_H        , KC_J        , KC_K        , KC_L        , KC_SCLN      , RCTL_T(KC_QUOT) ,
             KC_LSFT          , KC_Z          , KC_X        , KC_C                , KC_V                 , KC_B          , KC_N        , KC_M        , KC_COMM     , KC_DOT      , KC_SLSH      , RSFT_T(KC_ENT)  ,
-            FN1              , OSL3          , KC_LGUI     , KC_LALT             , ESC_FN2              , SPACE_FN3     , KC_GRAVE    , TT(_FN4)    , OSL3        , ENTER_FN2)
+            FN1              , OSL3          , KC_LGUI     , KC_LALT             , ESC_FN2              , SPACE_FN3     , TILDE_FN3    , TT(_FN4)    , BSLS_FN2        , PIPE_FN1)
 
                              ,
     [_FN1] = LAYOUT(
