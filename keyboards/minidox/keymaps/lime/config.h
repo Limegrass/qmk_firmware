a9a622c7ef4bd66ae36380d2849e310c47762fb6
Limegrass <multipresident@gmail.com>
2019-05-10 23:30:44 -0500

    Minidox config
    Initial minidox keymap and config

---
+++ keyboards/minidox/keymaps/lime/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+/* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TAP_TOGGLE 5
+/* Time (in ms) before the one shot key is released */
+#define ONESHOT_TIMEOUT 333
+
+#define TAPPING_TERM 120
+#define RETRO_TAPPING
+
+#endif
