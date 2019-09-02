4ca539b86d182323ddf617986377c3ae1da88d54
Limegrass <multipresident@gmail.com>
2019-09-01 21:19:10 -0500

    UT47.2 Keymaps and update to others for consistency

--- keyboards/dz60/keymaps/lime/keymap.c
+++ keyboards/dz60/keymaps/lime/keymap.c
@@ -54,45 +54,45 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
-        KC_GESC        , KC_1       , KC_2          , KC_3                , KC_4                 , KC_5          , KC_6           , KC_7       , KC_8       , KC_9       , KC_0       , KC_MINS       , KC_EQL         , KC_DEL  , KC_BSPC ,
-        KC_TAB         , KC_Q       , KC_W          , KC_E                , KC_R                 , KC_T          , KC_Y           , KC_U       , KC_I       , KC_O       , KC_P       , KC_LBRC       , KC_RBRC        , KC_BSLS ,
-        KC_LCTL        , KC_A       , KC_S          , KC_D                , KC_F                 , KC_G          , KC_H           , KC_J       , KC_K       , KC_L       , KC_SCLN    , KC_QUOT       , KC_ENT ,
-        OSM(MOD_LSFT)        , KC_LSFT    , KC_Z          , KC_X                , KC_C                 , KC_V          , KC_B           , KC_N       , KC_M       , KC_COMM    , KC_DOT     , KC_SLSH       , KC_ENT , KC_RSFT ,
-        FN1            , KC_LGUI    , KC_LALT       , ESC_FN2             , SPACE_FN3            , SPACE_FN2     , LALT(KC_GRAVE) , KC_BSLS    , XXXXXXX    , TT(_FN5)   , KC_RCTL)   ,
+        KC_GESC       , KC_1    , KC_2    , KC_3    , KC_4      , KC_5      , KC_6           , KC_7    , KC_8    , KC_9     , KC_0     , KC_MINS , KC_EQL  , KC_DEL  , KC_BSPC ,
+        KC_TAB        , KC_Q    , KC_W    , KC_E    , KC_R      , KC_T      , KC_Y           , KC_U    , KC_I    , KC_O     , KC_P     , KC_LBRC , KC_RBRC , KC_BSLS ,
+        KC_LCTL       , KC_A    , KC_S    , KC_D    , KC_F      , KC_G      , KC_H           , KC_J    , KC_K    , KC_L     , KC_SCLN  , KC_QUOT , KC_ENT  ,
+        OSM(MOD_LSFT) , KC_LSFT , KC_Z    , KC_X    , KC_C      , KC_V      , KC_B           , KC_N    , KC_M    , KC_COMM  , KC_DOT   , KC_SLSH , KC_ENT  , KC_RSFT ,
+        FN1           , KC_LGUI , KC_LALT , ESC_FN2 , SPACE_FN3 , SPACE_FN2 , LALT(KC_GRAVE) , KC_BSLS , XXXXXXX , TT(_FN5) , KC_RCTL) ,
 
     [_FN1] = LAYOUT(
-        KC_GRV         , KC_F1      , KC_F2         , KC_F3               , KC_F4                , KC_F5         , KC_F6          , KC_F7      , KC_F8      , KC_F9      , KC_F10     , KC_F11        , KC_F12         , _______ , KC_DEL  ,
-        KC_GRAVE       , KC_PIPE    , KC_BSLASH     , KC_LEFT_CURLY_BRACE , KC_RIGHT_CURLY_BRACE , KC_UNDERSCORE , _______        , _______    , KC_HOME    , KC_END     , KC_PGUP    , KC_PGDN       , KC_DEL         , _______ ,
-        _______        , KC_TILDE   , KC_UNDERSCORE , KC_LEFT_PAREN       , KC_RIGHT_PAREN       , KC_PLUS       , KC_LEFT        , KC_DOWN    , KC_UP      , KC_RGHT    , KC_INS     , KC_ENT        , _______        ,
-        _______        , _______    , KC_PLUS       , KC_UNDERSCORE       , KC_LBRC              , KC_RBRC       , _______        , _______    , KC_PSCREEN , KC_MUTE    , KC_VOLD    , KC_VOLU       , _______        , _______ ,
-        _______        , _______    , _______       , KC_COLON            , KC_BSPC              , KC_ENTER      , _______        , _______    , _______    , FN4        , _______)   ,
+        KC_GRV   , KC_F1    , KC_F2         , KC_F3               , KC_F4                , KC_F5         , KC_F6   , KC_F7   , KC_F8      , KC_F9   , KC_F10   , KC_F11  , KC_F12  , _______ , KC_DEL ,
+        KC_GRAVE , KC_PIPE  , KC_BSLASH     , KC_LEFT_CURLY_BRACE , KC_RIGHT_CURLY_BRACE , KC_UNDERSCORE , _______ , _______ , KC_PGUP    , KC_HOME , KC_END   , KC_PGDN , KC_DEL  , _______ ,
+        _______  , KC_TILDE , KC_UNDERSCORE , KC_LEFT_PAREN       , KC_RIGHT_PAREN       , KC_PLUS       , KC_LEFT , KC_DOWN , KC_UP      , KC_RGHT , KC_INS   , KC_ENT  , _______ ,
+        _______  , _______  , KC_PLUS       , KC_UNDERSCORE       , KC_LBRC              , KC_RBRC       , _______ , _______ , KC_PSCREEN , KC_MUTE , KC_VOLD  , KC_VOLU , _______ , _______ ,
+        _______  , _______  , _______       , KC_COLON            , KC_BSPC              , KC_ENTER      , _______ , _______ , _______    , FN4     , _______) ,
 
     [_FN2] = LAYOUT(
-        _______        , _______    , _______       , _______             , _______              , _______       , _______        , _______    , _______    , _______    , _______    , _______       , _______        , _______ , _______ ,
-        KC_EQL         , KC_1       , KC_2          , KC_3                , KC_4                 , KC_5          , KC_6           , KC_7       , KC_8       , KC_9       , KC_0       , KC_MINS       , KC_EQL         , _______ ,
-        _______        , LSFT(KC_1) , LSFT(KC_2)    , LSFT(KC_3)          , LSFT(KC_4)           , LSFT(KC_5)    , LSFT(KC_6)     , LSFT(KC_7) , LSFT(KC_8) , KC_PLUS    , KC_MINUS   , LSFT(KC_MINS) , LSFT(KC_EQL)   ,
-        _______        , _______    , LGUI(KC_1)    , LGUI(KC_2)          , LGUI(KC_3)           , LGUI(KC_4)    , LGUI(KC_5)     , LGUI(KC_6) , LGUI(KC_7) , _______ , _______ , _______    , KC_MPLY        , _______ ,
-        KC_BSPC        , _______    , _______       , _______             , KC_BSPC              , KC_DEL        , _______        , _______    , KC_MPRV    , KC_MSTP    , KC_MNXT)   ,
+        _______ , _______    , _______    , _______    , _______    , _______    , _______    , _______    , _______    , _______ , _______  , _______       , _______      , _______ , _______ ,
+        KC_EQL  , KC_1       , KC_2       , KC_3       , KC_4       , KC_5       , KC_6       , KC_7       , KC_8       , KC_9    , KC_0     , KC_MINS       , KC_EQL       , _______ ,
+        _______ , LSFT(KC_1) , LSFT(KC_2) , LSFT(KC_3) , LSFT(KC_4) , LSFT(KC_5) , LSFT(KC_6) , LSFT(KC_7) , LSFT(KC_8) , KC_PLUS , KC_MINUS , LSFT(KC_MINS) , LSFT(KC_EQL) ,
+        _______ , _______    , LGUI(KC_1) , LGUI(KC_2) , LGUI(KC_3) , LGUI(KC_4) , LGUI(KC_5) , LGUI(KC_6) , LGUI(KC_7) , _______ , _______  , _______       , KC_MPLY      , _______ ,
+        KC_BSPC , _______    , _______    , _______    , KC_BSPC    , KC_DEL     , _______    , _______    , KC_MPRV    , KC_MSTP , KC_MNXT) ,
 
     [_FN3] = LAYOUT(
-        _______        , _______    , _______       , _______             , _______              , _______       , _______        , _______    , _______    , _______    , _______    , _______       , _______        , _______ , _______ ,
-        _______        , KC_F1      , KC_F2         , KC_F3               , KC_F4                , KC_F5         , KC_F6          , KC_F7      , KC_F8      , KC_F9      , KC_F10     , KC_F11        , KC_F12         , _______ ,
-        _______        , _______    , _______       , _______             , _______              , _______       , _______        , _______    , _______    , _______    , _______    , _______       , _______        ,
-        _______        , _______    , _______       , _______             , _______              , _______       , _______        , _______    , _______    , _______    , _______    , _______       , _______        , _______ ,
-        _______        , _______    , _______       , _______             , _______              , _______       , _______        , _______    , _______    , _______    , _______)   ,
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______  , _______ , _______ , _______ , _______ ,
+        _______ , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10   , KC_F11  , KC_F12  , _______ ,
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______  , _______ , _______ ,
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______  , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______) ,
 
     [_FN4] = LAYOUT(
-        _______        , KC_F1      , KC_F2         , KC_F3               , KC_F4                , KC_F5         , KC_F6          , KC_F7      , KC_F8      , KC_F9      , KC_F10     , KC_F11        , KC_F12         , _______ , KC_DEL  ,
-        _______        , RGB_TOG    , RGB_MOD       , RGB_HUI             , RGB_HUD              , RGB_SAI       , RGB_SAD        , RGB_VAI    , RGB_VAD    , _______    , _______    , _______       , _______        , RESET   ,
-        _______        , _______    , _______       , _______             , _______              , _______       , _______        , _______    , _______    , _______    , _______    , _______       , _______        ,
-        _______        , _______    , _______       , _______             , BL_DEC               , BL_TOGG       , BL_INC         , BL_STEP    , _______    , _______    , _______    , _______       , _______        , _______ ,
-        _______        , _______    , _______       , _______             , _______              , _______       , _______        , _______    , _______    , _______    , _______)   ,
+        _______ , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10   , KC_F11  , KC_F12  , _______ , KC_DEL ,
+        _______ , RGB_TOG , RGB_MOD , RGB_HUI , RGB_HUD , RGB_SAI , RGB_SAD , RGB_VAI , RGB_VAD , _______ , _______  , _______ , _______ , RESET   ,
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______  , _______ , _______ ,
+        _______ , _______ , _______ , _______ , BL_DEC  , BL_TOGG , BL_INC  , BL_STEP , _______ , _______ , _______  , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______) ,
 
     // Gaming/PS layer
     [_FN5] = LAYOUT(
-        _______        , _______    , _______       , _______             , _______              , _______       , _______        , _______    , _______    , _______    , _______    , _______       , _______        , _______ , _______ ,
-        _______        , _______    , _______       , _______             , _______              , _______       , _______        , _______    , _______    , _______    , _______    , _______       , _______        , _______ ,
-        _______        , _______    , _______       , _______             , _______              , _______       , _______        , _______    , _______    , _______    , _______    , _______       , _______        ,
-        _______        , _______    , _______       , _______             , _______              , _______       , _______        , _______    , _______    , _______    , _______    , _______       , _______        , _______ ,
-        _______        , _______    , _______       , KC_SPACE            , _______             , _______      , _______        , _______    , _______    , _______    , _______)   ,
+        _______ , _______ , _______ , _______  , _______ , _______ , _______ , _______ , _______ , _______ , _______  , _______ , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______  , _______ , _______ , _______ , _______ , _______ , _______ , _______  , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______  , _______ , _______ , _______ , _______ , _______ , _______ , _______  , _______ , _______ ,
+        _______ , _______ , _______ , _______  , _______ , _______ , _______ , _______ , _______ , _______ , _______  , _______ , _______ , _______ ,
+        _______ , _______ , _______ , KC_SPACE , _______ , KC_BSPC , _______ , _______ , _______ , _______ , _______) ,
 };
