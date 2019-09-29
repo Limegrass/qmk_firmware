f20c10cded2e7cb34c0bab505311d8b39f13dbe3
Limegrass <multipresident@gmail.com>
2019-09-29 01:53:16 -0500

    Removed OSM functionality from LSFT

--- keyboards/ut472/keymaps/lime/keymap.c
+++ keyboards/ut472/keymaps/lime/keymap.c
@@ -23,10 +23,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-------------------------------------------------------------------------'
      */
     [_BASE] = LAYOUT(
-            KC_TAB        , KC_Q , KC_W    , KC_E    , KC_R    , KC_T     , KC_Y       , KC_U           , KC_I          , KC_O    , KC_P          , KC_BSPC ,
-            KC_LCTRL      , KC_A , KC_S    , KC_D    , KC_F    , KC_G     , KC_H       , KC_J           , KC_K          , KC_L    , KC_SCLN       , KC_QUOTE ,
-            OSM(MOD_LSFT) , KC_Z , KC_X    , KC_C    , KC_V    , KC_B     , KC_N       , KC_M           , KC_COMM       , KC_DOT  , KC_SLSH       , KC_ENT   ,
-            MO(_MOTION)   , OSL(_FUNCTION) , KC_LGUI , KC_LALT , ESC_NUM , KC_SPACE    , SPACE_FUNC , LALT(KC_GRAVE) , TT(_HARDWARE) , KC_APP , MO(_HARDWARE)
+            KC_TAB      , KC_Q           , KC_W    , KC_E    , KC_R    , KC_T     , KC_Y       , KC_U           , KC_I          , KC_O   , KC_P          , KC_BSPC  ,
+            KC_LCTRL    , KC_A           , KC_S    , KC_D    , KC_F    , KC_G     , KC_H       , KC_J           , KC_K          , KC_L   , KC_SCLN       , KC_QUOTE ,
+            KC_LSFT     , KC_Z           , KC_X    , KC_C    , KC_V    , KC_B     , KC_N       , KC_M           , KC_COMM       , KC_DOT , KC_SLSH       , KC_ENT   ,
+            MO(_MOTION) , OSL(_FUNCTION) , KC_LGUI , KC_LALT , ESC_NUM , KC_SPACE , SPACE_FUNC , LALT(KC_GRAVE) , TT(_HARDWARE) , KC_APP , MO(_HARDWARE)
             ),
 
 
