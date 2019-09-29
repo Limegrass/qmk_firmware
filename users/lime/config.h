93c65ce2ababdb3363b317c7764b5ec359797b37
Limegrass <multipresident@gmail.com>
2019-09-29 01:54:15 -0500

    Modified mouse functionality to be slower and easier controlled
    Removed acceleration on mouse and lowered the max speed.
    Lowered the internal for smoother movement.

--- users/lime/config.h
+++ users/lime/config.h
@@ -12,10 +12,10 @@
 #define RETRO_TAPPING
 
 #define MOUSEKEY_DELAY             100
-#define MOUSEKEY_INTERVAL          25
-#define MOUSEKEY_MAX_SPEED         10
-#define MOUSEKEY_TIME_TO_MAX       20
-#define MOUSEKEY_WHEEL_MAX_SPEED   5
+#define MOUSEKEY_INTERVAL          5
+#define MOUSEKEY_MAX_SPEED         1
+#define MOUSEKEY_TIME_TO_MAX       0
+#define MOUSEKEY_WHEEL_MAX_SPEED   1
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 20
 
 #endif
