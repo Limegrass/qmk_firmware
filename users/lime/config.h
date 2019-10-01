3996333d1b56b1efe95be54cf3388a955d689e63
Limegrass <multipresident@gmail.com>
2019-09-30 21:14:34 -0500

    Added acceleration back as no-accel could not do precise movements.

--- users/lime/config.h
+++ users/lime/config.h
@@ -13,8 +13,8 @@
 
 #define MOUSEKEY_DELAY             0
 #define MOUSEKEY_INTERVAL          5
-#define MOUSEKEY_MAX_SPEED         1
-#define MOUSEKEY_TIME_TO_MAX       0
+#define MOUSEKEY_MAX_SPEED         2
+#define MOUSEKEY_TIME_TO_MAX       33
 #define MOUSEKEY_WHEEL_MAX_SPEED   1
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 20
 
