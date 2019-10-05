458154733b220ad54495773119a10947ae1e22ec
Limegrass <multipresident@gmail.com>
2019-10-05 01:29:45 -0500

    Updated mouse key button layout on UT and Laplace

--- keyboards/ut472/keymaps/lime/keymap.c
+++ keyboards/ut472/keymaps/lime/keymap.c
@@ -31,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     [_MOTION] = LAYOUT(
-            KC_GRAVE , KC_PIPE  , KC_BSLASH       , KC_LEFT_CURLY_BRACE , KC_RIGHT_CURLY_BRACE , KC_UNDERSCORE , _______ , KC_PGUP , KC_HOME    , KC_END  , KC_PGDN , KC_DEL ,
-            KC_RCTRL  , KC_TILDE , KC_UNDERSCORE   , KC_LEFT_PAREN       , KC_RIGHT_PAREN       , KC_PLUS       , KC_LEFT , KC_DOWN , KC_UP      , KC_RGHT , KC_INS  , KC_ENT  ,
+            KC_GRAVE , KC_PIPE  , KC_BSLASH       , KC_LEFT_CURLY_BRACE , KC_RIGHT_CURLY_BRACE , KC_UNDERSCORE , _______ , KC_PGUP , KC_HOME    , KC_END  , KC_PGDN , KC_DEL  ,
+            KC_RCTRL , KC_TILDE , KC_UNDERSCORE   , KC_LEFT_PAREN       , KC_RIGHT_PAREN       , KC_PLUS       , KC_LEFT , KC_DOWN , KC_UP      , KC_RGHT , KC_INS  , KC_ENT  ,
             KC_LSFT  , KC_QUOTE , KC_DOUBLE_QUOTE , KC_LBRC             , KC_RBRC              , _______       , _______ , _______ , KC_PSCREEN , KC_MUTE , KC_VOLD , KC_VOLU ,
             _______  , _______  , KC_LGUI         , KC_LALT             , _______              , _______       , _______ , KC_MPRV , KC_MPLY    , KC_MSTP , KC_MNXT
             ),
@@ -48,14 +48,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_F12   , KC_F1       , KC_F2       , KC_F3       , KC_F4       , KC_F5       , KC_F6       , KC_F7       , KC_F8       , KC_F9       , KC_F10       , KC_F11       ,
             KC_LCTRL , LGUI(KC_F1) , LGUI(KC_F2) , LGUI(KC_F3) , LGUI(KC_F4) , LGUI(KC_F5) , LGUI(KC_F6) , LGUI(KC_F7) , LGUI(KC_F8) , LGUI(KC_F9) , LGUI(KC_F10) , LGUI(KC_F11) ,
             KC_LSFT  , LALT(KC_F1) , LALT(KC_F2) , LALT(KC_F3) , LALT(KC_F4) , LALT(KC_F5) , LALT(KC_F6) , LALT(KC_F7) , LALT(KC_F8) , LALT(KC_F9) , LALT(KC_F10) , LALT(KC_F11) ,
-            KC_RCTRL  , KC_CAPSLOCK , KC_LGUI     , KC_LALT     , _______     , _______     , _______     , KC_LEFT     , KC_DOWN     , KC_UP       , KC_RGHT
+            KC_RCTRL , KC_CAPSLOCK , KC_LGUI     , KC_LALT     , _______     , _______     , _______     , KC_LEFT     , KC_DOWN     , KC_UP       , KC_RGHT
             ),
 
     [_HARDWARE] = LAYOUT(
-            RESET , KC_MS_BTN1 , KC_MS_UP   , KC_MS_BTN2  , KC_MS_WH_UP   , _______ , KC_PAST , KC_7    , KC_8    , KC_9    , KC_EQL  , KC_BSPC ,
-            KC_LCTL , KC_MS_LEFT , KC_MS_DOWN , KC_MS_RIGHT , KC_MS_WH_DOWN , _______ , KC_SLSH , KC_4    , KC_5    , KC_6    , KC_MINS , KC_ENT  ,
-            KC_LSFT , KC_MS_BTN3 , KC_MS_BTN4 , KC_MS_BTN5  , KC_MS_WH_LEFT , KC_MS_WH_RIGHT , KC_DOT  , KC_1    , KC_2    , KC_3    , KC_UP   , KC_DOWN ,
-            RGB_TOG , RGB_MOD    , KC_LGUI    , KC_LALT     , _______       , KC_0    , KC_TAB  , _______ , KC_LEFT , KC_RGHT, _______
+            _______ , KC_MS_BTN1    , KC_MS_UP   , KC_MS_BTN2     , KC_MS_WH_UP   , _______    , KC_PAST , KC_7    , KC_8  , KC_9    , KC_EQL           , KC_BSPC            ,
+            KC_LCTL , KC_MS_LEFT    , KC_MS_DOWN , KC_MS_RIGHT    , KC_MS_WH_DOWN , _______    , KC_SLSH , KC_4    , KC_5  , KC_6    , KC_MINS          , KC_ENT             ,
+            KC_LSFT , KC_MS_WH_LEFT , KC_MS_BTN3 , KC_MS_WH_RIGHT , KC_MS_BTN4    , KC_MS_BTN5 , KC_DOT  , KC_1    , KC_2  , KC_3    , KC_BRIGHTNESS_DOWN , KC_BRIGHTNESS_UP  ,
+            RGB_TOG , RGB_MOD       , KC_LGUI    , KC_LALT        , _______       , KC_0       , KC_TAB  , _______ , RESET , _______ , _______
             )
 
 };
