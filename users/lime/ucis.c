b869b7826b4aa700e947ea72471eb2cbbc8aa244
Limegrass <multipresident@gmail.com>
2019-10-05 01:30:37 -0500

    Added unicode (emojis) definition files

---
+++ users/lime/ucis.c
@@ -0,0 +1,7 @@
+#include "ucis.h"
+
+const qk_ucis_symbol_t ucis_symbol_table[] = UCIS_TABLE(
+    UCIS_SYM("poop", 0x1F4A9), // 💩
+    UCIS_SYM("rofl", 0x1F923), // 🤣
+    UCIS_SYM("kiss", 0x1F619)  // 😙
+);
