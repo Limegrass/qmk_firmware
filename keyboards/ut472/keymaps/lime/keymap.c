7b7923e8c394a8ab8a8b70c96986713bf5ce4b2f
Limegrass <multipresident@gmail.com>
2019-12-14 13:46:46 -0600

    Added game layers to UT472

--- keyboards/ut472/keymaps/lime/keymap.c
+++ keyboards/ut472/keymaps/lime/keymap.c
@@ -5,6 +5,9 @@
 #define _NUMBER 2
 #define _FUNCTION 3
 #define _HARDWARE 4
+#define _GAMEBASE 5
+#define _GAMENUMBER 6
+#define _GAMEFUNCTION 7
 
 #define ESC_NUM LT(_NUMBER , KC_ESCAPE)
 #define SPACE_FUNC LT(_FUNCTION , KC_SPACE)
@@ -31,10 +34,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     [_MOTION] = LAYOUT(
-            KC_GRAVE , KC_PIPE  , KC_BSLASH       , KC_LEFT_CURLY_BRACE , KC_RIGHT_CURLY_BRACE , KC_UNDERSCORE , _______ , KC_PGUP , KC_HOME    , KC_END  , KC_PGDN , KC_DEL  ,
-            KC_RCTRL , KC_TILDE , KC_UNDERSCORE   , KC_LEFT_PAREN       , KC_RIGHT_PAREN       , KC_PLUS       , KC_LEFT , KC_DOWN , KC_UP      , KC_RGHT , KC_INS  , KC_ENT  ,
-            KC_LSFT  , KC_QUOTE , KC_DOUBLE_QUOTE , KC_LBRC             , KC_RBRC              , _______       , _______ , _______ , KC_PSCREEN , KC_MUTE , KC_VOLD , KC_VOLU ,
-            _______  , _______  , KC_LGUI         , KC_LALT             , _______              , _______       , _______ , KC_MPRV , KC_MPLY    , KC_MSTP , KC_MNXT
+            KC_GRAVE , KC_PIPE  , KC_BSLASH       , KC_LEFT_CURLY_BRACE , KC_RIGHT_CURLY_BRACE , KC_UNDERSCORE , _______       , KC_PGUP , KC_HOME    , KC_END  , KC_PGDN  , KC_DEL  ,
+            KC_RCTRL , KC_TILDE , KC_UNDERSCORE   , KC_LEFT_PAREN       , KC_RIGHT_PAREN       , KC_PLUS       , KC_LEFT       , KC_DOWN , KC_UP      , KC_RGHT , KC_INS   , KC_ENT  ,
+            KC_LSFT  , KC_QUOTE , KC_DOUBLE_QUOTE , KC_LBRC             , KC_RBRC              , _______       , _______       , _______ , KC_PSCREEN , KC_MUTE , KC_VOLD  , KC_VOLU ,
+            _______  , _______  , KC_LGUI         , KC_LALT             , _______              , _______       , DF(_GAMEBASE) , KC_MPRV , KC_MPLY    , KC_MSTP , KC_MNXT
             ),
 
     [_NUMBER] = LAYOUT(
@@ -52,10 +55,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             ),
 
     [_HARDWARE] = LAYOUT(
-            _______ , KC_MS_BTN1    , KC_MS_UP   , KC_MS_BTN2     , KC_MS_WH_UP   , _______    , KC_PAST , KC_7    , KC_8  , KC_9    , KC_EQL           , KC_BSPC            ,
-            KC_LCTL , KC_MS_LEFT    , KC_MS_DOWN , KC_MS_RIGHT    , KC_MS_WH_DOWN , _______    , KC_SLSH , KC_4    , KC_5  , KC_6    , KC_MINS          , KC_ENT             ,
-            KC_LSFT , KC_MS_WH_LEFT , KC_MS_BTN3 , KC_MS_WH_RIGHT , KC_MS_BTN4    , KC_MS_BTN5 , KC_DOT  , KC_1    , KC_2  , KC_3    , KC_BRIGHTNESS_DOWN , KC_BRIGHTNESS_UP  ,
+            _______ , KC_MS_BTN1    , KC_MS_UP   , KC_MS_BTN2     , KC_MS_WH_UP   , _______    , KC_PAST , KC_7    , KC_8  , KC_9    , KC_EQL             , KC_BSPC          ,
+            KC_LCTL , KC_MS_LEFT    , KC_MS_DOWN , KC_MS_RIGHT    , KC_MS_WH_DOWN , _______    , KC_SLSH , KC_4    , KC_5  , KC_6    , KC_MINS            , KC_ENT           ,
+            KC_LSFT , KC_MS_WH_LEFT , KC_MS_BTN3 , KC_MS_WH_RIGHT , KC_MS_BTN4    , KC_MS_BTN5 , KC_DOT  , KC_1    , KC_2  , KC_3    , KC_BRIGHTNESS_DOWN , KC_BRIGHTNESS_UP ,
             RGB_TOG , RGB_MOD       , KC_LGUI    , KC_LALT        , _______       , KC_0       , KC_TAB  , _______ , RESET , _______ , _______
-            )
+            ),
 
+    [_GAMEBASE] = LAYOUT(
+            KC_TAB          , KC_Q              , KC_W    , KC_E    , KC_R     , KC_T     , KC_Y             , KC_U       , KC_I           , KC_O      , KC_P    , KC_BSPC        ,
+            KC_LCTRL        , KC_A              , KC_S    , KC_D    , KC_F     , KC_G     , KC_H             , KC_J       , KC_K           , KC_L      , KC_SCLN , KC_QUOTE       ,
+            KC_LSFT         , KC_Z              , KC_X    , KC_C    , KC_V     , KC_B     , KC_N             , KC_M       , KC_COMM        , KC_DOT    , KC_SLSH , KC_ENT         ,
+            MO(_GAMENUMBER) , MO(_GAMEFUNCTION) , KC_LGUI , KC_LALT , KC_SPACE , KC_SPACE , LT(_GAMEFUNCTION , KC_ESCAPE) , LALT(KC_GRAVE) , DF(_BASE) , KC_APP  , MO(_HARDWARE)
+            ),
+
+    [_GAMENUMBER] = LAYOUT(
+            KC_GRAVE , KC_1               , KC_2    , KC_3    , KC_4    , _______ , _______   , KC_PGUP , KC_HOME    , KC_END  , KC_PGDN  , KC_DEL  ,
+            _______  , KC_5               , KC_6    , KC_7    , KC_8    , _______ , KC_LEFT   , KC_DOWN , KC_UP      , KC_RGHT , KC_INS   , KC_ENT  ,
+            _______  , KC_9               , KC_0    , KC_EQL  , _______ , _______ , _______   , _______ , KC_PSCREEN , KC_MUTE , KC_VOLD  , KC_VOLU ,
+            _______  , OSL(_GAMEFUNCTION) , _______ , _______ , _______ , _______ , DF(_BASE) , KC_MPRV , KC_MPLY    , KC_MSTP , KC_MNXT
+            ),
+
+    [_GAMEFUNCTION] = LAYOUT(
+            KC_GRAVE , KC_F1   , KC_F2   , KC_F3   , KC_F4   , _______ , _______   , _______ , _______ , _______ , _______  , _______ ,
+            _______  , KC_F5   , KC_F6   , KC_F7   , KC_F8   , _______ , _______   , _______ , _______ , _______ , _______  , _______ ,
+            _______  , KC_F9   , KC_F10  , KC_F11  , KC_F12  , _______ , _______   , _______ , _______ , _______ , _______  , _______ ,
+            _______  , _______ , _______ , _______ , _______ , _______ , DF(_BASE) , _______ , _______ , _______ , _______
+            )
 };
