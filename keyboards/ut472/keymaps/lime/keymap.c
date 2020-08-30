bc6b43abf496db5f04892d983dbe232954329058
James Ni <james@niis.me>
2020-08-30 11:58:52 -0700

    Swapped enter and esc in Game, Esc to App in game

--- keyboards/ut472/keymaps/lime/keymap.c
+++ keyboards/ut472/keymaps/lime/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB          , KC_Q              , KC_W    , KC_E    , KC_R     , KC_T     , KC_Y             , KC_U       , KC_I           , KC_O      , KC_P    , KC_BSPC        ,
             KC_LCTRL        , KC_A              , KC_S    , KC_D    , KC_F     , KC_G     , KC_H             , KC_J       , KC_K           , KC_L      , KC_SCLN , KC_QUOTE       ,
             KC_LSFT         , KC_Z              , KC_X    , KC_C    , KC_V     , KC_B     , KC_N             , KC_M       , KC_COMM        , KC_DOT    , KC_SLSH , KC_RSFT        ,
-            MO(_GAMENUMBER) , MO(_GAMEFUNCTION) , KC_LGUI , KC_LALT , KC_SPACE , KC_SPACE , LT(_GAMEFUNCTION , KC_ESCAPE) , KC_RALT        , DF(_BASE) , APP_GUI  , TT(_HARDWARE)
+            MO(_GAMENUMBER) , MO(_GAMEFUNCTION) , KC_LGUI , KC_LALT , KC_SPACE , KC_SPACE , LT(_GAMEFUNCTION , KC_ENTER)  , KC_RALT        , DF(_BASE) , KC_ESCAPE  , TT(_HARDWARE)
             ),
 
     [_GAMENUMBER] = LAYOUT(
