bbe0477d995ce4e082632eb09403e782fe6e63f9
limegrass <james@niis.me>
2023-02-13 23:11:24 -0800

    refactor(ut472:lime): remove OSM for pure key strokes
    in case it adds any additional delay

--- keyboards/keyhive/ut472/keymaps/lime/keymap.c
+++ keyboards/keyhive/ut472/keymaps/lime/keymap.c
@@ -31,14 +31,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
             KC_TAB        , KC_Q           , KC_W    , KC_E    , KC_R    , KC_T     , KC_Y       , KC_U    , KC_I          , KC_O    , KC_P          , KC_BSPC       ,
             KC_LCTL       , KC_A           , KC_S    , KC_D    , KC_F    , KC_G     , KC_H       , KC_J    , KC_K          , KC_L    , KC_SCLN       , KC_QUOTE      ,
-            OSM(MOD_LSFT) , KC_Z           , KC_X    , KC_C    , KC_V    , KC_B     , KC_N       , KC_M    , KC_COMM       , KC_DOT  , KC_SLSH       , OSM(MOD_RSFT) ,
+            KC_LEFT_SHIFT , KC_Z           , KC_X    , KC_C    , KC_V    , KC_B     , KC_N       , KC_M    , KC_COMM       , KC_DOT  , KC_SLSH       , KC_RIGHT_SHIFT ,
             MO(_MOTION)   , OSL(_FUNCTION) , KC_LGUI , KC_LALT , ESC_NUM , KC_SPACE , ENTER_FUNC , KC_RALT , TT(_HARDWARE) , APP_GUI , TT(_HARDWARE)
             )             ,
 
     [_WORKMAN] = LAYOUT(
             KC_TAB        , KC_Q           , KC_D    , KC_R    , KC_W    , KC_B     , KC_J       , KC_F    , KC_U          , KC_P    , KC_SCLN       , KC_BSPC       ,
             KC_LCTL       , KC_A           , KC_S    , KC_H    , KC_T    , KC_G     , KC_Y       , KC_N    , KC_E          , KC_O    , KC_I          , KC_QUOTE      ,
-            OSM(MOD_LSFT) , KC_Z           , KC_X    , KC_M    , KC_C    , KC_V     , KC_K       , KC_L    , KC_COMM       , KC_DOT  , KC_SLSH       , OSM(MOD_RSFT) ,
+            KC_LEFT_SHIFT , KC_Z           , KC_X    , KC_M    , KC_C    , KC_V     , KC_K       , KC_L    , KC_COMM       , KC_DOT  , KC_SLSH       , KC_RIGHT_SHIFT ,
             MO(_MOTION)   , OSL(_FUNCTION) , KC_LGUI , KC_LALT , ESC_NUM , KC_SPACE , ENTER_FUNC , KC_RALT , TT(_HARDWARE) , APP_GUI , TT(_HARDWARE)
             )             ,
 
