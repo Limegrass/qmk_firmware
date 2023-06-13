7fa6cc7497d052cf5cf426dcce232db5db442b63
Limegrass <james@niis.me>
2023-06-12 18:34:33 -0700

    fix(planck/rev7:lime): move right GUI closer to right thumb

--- keyboards/planck/rev7/keymaps/lime/keymap.c
+++ keyboards/planck/rev7/keymaps/lime/keymap.c
@@ -40,14 +40,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB        , KC_Q           , KC_W    , KC_E    , KC_R    , KC_T     , KC_Y       , KC_U    , KC_I          , KC_O    , KC_P          , KC_BSPC       ,
             KC_LCTL       , KC_A           , KC_S    , KC_D    , KC_F    , KC_G     , KC_H       , KC_J    , KC_K          , KC_L    , KC_SCLN       , KC_QUOTE      ,
             KC_LEFT_SHIFT , KC_Z           , KC_X    , KC_C    , KC_V    , KC_B     , KC_N       , KC_M    , KC_COMM       , KC_DOT  , KC_SLSH       , KC_RIGHT_SHIFT ,
-            MO(_MOTION)   , OSL(_FUNCTION) , KC_LGUI , KC_LALT , ESC_NUM , KC_SPACE , ENTER_FUNC , KC_RALT , TT(_HARDWARE) , APP_GUI , TT(_HARDWARE)
+            MO(_MOTION)   , OSL(_FUNCTION) , KC_LGUI , KC_LALT , ESC_NUM , KC_SPACE , ENTER_FUNC , KC_RALT , APP_GUI       , APP_GUI , TT(_HARDWARE)
             )             ,
 
     [_COLEMAK_DHM] = LAYOUT_planck_mit(
             KC_TAB        , KC_Q           , KC_W    , KC_F    , KC_P    , KC_B     , KC_J       , KC_L    , KC_U          , KC_Y    , KC_SCLN       , KC_BSPC       ,
             KC_LCTL       , KC_A           , KC_R    , KC_S    , KC_T    , KC_G     , KC_M       , KC_N    , KC_E          , KC_I    , KC_O          , KC_QUOTE      ,
             KC_LEFT_SHIFT , KC_Z           , KC_X    , KC_C    , KC_D    , KC_V     , KC_K       , KC_H    , KC_COMM       , KC_DOT  , KC_SLSH       , KC_RIGHT_SHIFT ,
-            MO(_MOTION)   , OSL(_FUNCTION) , KC_LGUI , KC_LALT , ESC_NUM , KC_SPACE , ENTER_FUNC , KC_RALT , TT(_HARDWARE) , APP_GUI , TT(_HARDWARE)
+            MO(_MOTION)   , OSL(_FUNCTION) , KC_LGUI , KC_LALT , ESC_NUM , KC_SPACE , ENTER_FUNC , KC_RALT , APP_GUI       , APP_GUI , TT(_HARDWARE)
             )             ,
 
     [_MOTION] = LAYOUT_planck_mit(
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             )             ,
 
     [_HARDWARE] = LAYOUT_planck_mit(
-            XXXXXXX       , KC_MS_BTN1    , KC_MS_UP   , KC_MS_BTN2     , KC_MS_WH_UP   , _______      , XXXXXXX       , XXXXXXX  , XXXXXXX       , XXXXXXX , XXXXXXX            , QK_BOOTLOADER    ,
+            XXXXXXX       , KC_MS_BTN1    , KC_MS_UP   , KC_MS_BTN2     , KC_MS_WH_UP   , _______      , XXXXXXX       , XXXXXXX  , XXXXXXX       , XXXXXXX , KC_CAPS_LOCK       , QK_BOOTLOADER    ,
             KC_LCTL       , KC_MS_LEFT    , KC_MS_DOWN , KC_MS_RIGHT    , KC_MS_WH_DOWN , _______      , XXXXXXX       , XXXXXXX  , XXXXXXX       , XXXXXXX , XXXXXXX            , XXXXXXX          ,
             KC_LSFT       , KC_MS_WH_LEFT , KC_MS_BTN3 , KC_MS_WH_RIGHT , KC_MS_BTN4    , KC_MS_BTN5   , XXXXXXX       , XXXXXXX  , XXXXXXX       , XXXXXXX , KC_BRIGHTNESS_DOWN , KC_BRIGHTNESS_UP ,
             RGB_TOG       , RGB_MOD       , KC_LGUI    , KC_LALT        , QWERTY     , COLEMAK_DHM , DF(_GAMEBASE) , DF(_FPS) , QK_BOOTLOADER , XXXXXXX , QK_BOOTLOADER
