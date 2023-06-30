4a739834082f4b72a893bfa834dcbd0951060bd7
Limegrass <james@niis.me>
2023-06-30 08:59:33 -0700

    chore(ut472:lime): mirror changes made for Planck
    cb16fcc4baf174b7ca5bb8c630143e14d6b55262 and
    e2a4faddc848f6ce30a389328ed457083e44f7da for ut472
    and Workman to Colemak DHM

--- keyboards/keyhive/ut472/keymaps/lime/keymap.c
+++ keyboards/keyhive/ut472/keymaps/lime/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 
 #define _BASE 0
-#define _WORKMAN 1
+#define _COLEMAK_DHM 1
 #define _MOTION 2
 #define _NUMBER 3
 #define _FUNCTION 4
@@ -32,26 +32,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB        , KC_Q           , KC_W    , KC_E    , KC_R    , KC_T     , KC_Y       , KC_U    , KC_I          , KC_O    , KC_P          , KC_BSPC       ,
             KC_LCTL       , KC_A           , KC_S    , KC_D    , KC_F    , KC_G     , KC_H       , KC_J    , KC_K          , KC_L    , KC_SCLN       , KC_QUOTE      ,
             KC_LEFT_SHIFT , KC_Z           , KC_X    , KC_C    , KC_V    , KC_B     , KC_N       , KC_M    , KC_COMM       , KC_DOT  , KC_SLSH       , KC_RIGHT_SHIFT ,
-            MO(_MOTION)   , OSL(_FUNCTION) , KC_LGUI , KC_LALT , ESC_NUM , KC_SPACE , ENTER_FUNC , KC_RALT , TT(_HARDWARE) , APP_GUI , TT(_HARDWARE)
+            MO(_MOTION)   , OSL(_FUNCTION) , KC_LGUI , KC_LALT , ESC_NUM , KC_SPACE , ENTER_FUNC , KC_RALT , APP_GUI       , APP_GUI , TT(_HARDWARE)
             )             ,
 
-    [_WORKMAN] = LAYOUT(
-            KC_TAB        , KC_Q           , KC_D    , KC_R    , KC_W    , KC_B     , KC_J       , KC_F    , KC_U          , KC_P    , KC_SCLN       , KC_BSPC       ,
-            KC_LCTL       , KC_A           , KC_S    , KC_H    , KC_T    , KC_G     , KC_Y       , KC_N    , KC_E          , KC_O    , KC_I          , KC_QUOTE      ,
-            KC_LEFT_SHIFT , KC_Z           , KC_X    , KC_M    , KC_C    , KC_V     , KC_K       , KC_L    , KC_COMM       , KC_DOT  , KC_SLSH       , KC_RIGHT_SHIFT ,
-            MO(_MOTION)   , OSL(_FUNCTION) , KC_LGUI , KC_LALT , ESC_NUM , KC_SPACE , ENTER_FUNC , KC_RALT , TT(_HARDWARE) , APP_GUI , TT(_HARDWARE)
+    [_COLEMAK_DHM] = LAYOUT(
+            KC_TAB        , KC_Q           , KC_W    , KC_F    , KC_P    , KC_B     , KC_J       , KC_L    , KC_U          , KC_Y    , KC_SCLN       , KC_BSPC       ,
+            KC_LCTL       , KC_A           , KC_R    , KC_S    , KC_T    , KC_G     , KC_M       , KC_N    , KC_E          , KC_I    , KC_O          , KC_QUOTE      ,
+            KC_LEFT_SHIFT , KC_Z           , KC_X    , KC_C    , KC_D    , KC_V     , KC_K       , KC_H    , KC_COMM       , KC_DOT  , KC_SLSH       , KC_RIGHT_SHIFT ,
+            MO(_MOTION)   , OSL(_FUNCTION) , KC_LGUI , KC_LALT , ESC_NUM , KC_SPACE , ENTER_FUNC , KC_RALT , APP_GUI       , APP_GUI , TT(_HARDWARE)
             )             ,
 
     [_MOTION] = LAYOUT(
             KC_GRAVE    , KC_PIPE  , KC_BACKSLASH    , KC_LEFT_CURLY_BRACE , KC_RIGHT_CURLY_BRACE , KC_UNDERSCORE , _______ , KC_PGUP , KC_HOME , KC_END  , KC_PGDN , KC_DEL  ,
             KC_RCTL     , KC_TILDE , KC_UNDERSCORE   , KC_LEFT_PAREN       , KC_RIGHT_PAREN       , KC_QUOTE      , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_INS  , KC_ENT  ,
-            KC_LSFT     , KC_QUOTE , KC_DOUBLE_QUOTE , KC_LBRC             , KC_RBRC              , _______       , _______ , _______ , KC_PSCR , KC_MUTE , KC_VOLD , KC_VOLU ,
+            KC_LSFT     , KC_PLUS  , KC_MINUS        , KC_LBRC             , KC_RBRC              , _______       , _______ , _______ , KC_PSCR , KC_MUTE , KC_VOLD , KC_VOLU ,
             _______     , _______  , KC_LGUI         , KC_LALT             , _______              , _______       , _______ , KC_MPRV , KC_MPLY , KC_MSTP , KC_MNXT
             )           ,
 
     [_NUMBER] = LAYOUT(
-            KC_EQL  , KC_1       , KC_2       , KC_3       , KC_4       , KC_5       , KC_6       , KC_7       , KC_8       , KC_9       , KC_0       , KC_BSPC ,
-            KC_LCTL , LSFT(KC_1) , LSFT(KC_2) , LSFT(KC_3) , LSFT(KC_4) , LSFT(KC_5) , LSFT(KC_6) , LSFT(KC_7) , LSFT(KC_8) , KC_PLUS    , KC_MINUS   , KC_ENT  ,
+            KC_EQL  , LSFT(KC_1) , LSFT(KC_2) , LSFT(KC_3) , LSFT(KC_4) , LSFT(KC_5) , LSFT(KC_6) , LSFT(KC_7) , LSFT(KC_8) , KC_PLUS    , KC_MINUS   , KC_BSPC ,
+            KC_LCTL , KC_1       , KC_2       , KC_3       , KC_4       , KC_5       , KC_6       , KC_7       , KC_8       , KC_9       , KC_0        , KC_ENT  ,
             KC_LSFT , LGUI(KC_1) , LGUI(KC_2) , LGUI(KC_3) , LGUI(KC_4) , LGUI(KC_5) , LGUI(KC_6) , LGUI(KC_7) , LGUI(KC_8) , LGUI(KC_9) , LGUI(KC_0) , KC_RSFT ,
             _______ , _______    , KC_LGUI    , KC_LALT    , _______    , _______    , _______    , _______    , _______    , _______    , _______
             ),
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             XXXXXXX       , KC_MS_BTN1    , KC_MS_UP   , KC_MS_BTN2     , KC_MS_WH_UP   , _______      , XXXXXXX       , XXXXXXX  , XXXXXXX       , XXXXXXX , XXXXXXX            , QK_BOOTLOADER    ,
             KC_LCTL       , KC_MS_LEFT    , KC_MS_DOWN , KC_MS_RIGHT    , KC_MS_WH_DOWN , _______      , XXXXXXX       , XXXXXXX  , XXXXXXX       , XXXXXXX , XXXXXXX            , XXXXXXX          ,
             KC_LSFT       , KC_MS_WH_LEFT , KC_MS_BTN3 , KC_MS_WH_RIGHT , KC_MS_BTN4    , KC_MS_BTN5   , XXXXXXX       , XXXXXXX  , XXXXXXX       , XXXXXXX , KC_BRIGHTNESS_DOWN , KC_BRIGHTNESS_UP ,
-            RGB_TOG       , RGB_MOD       , KC_LGUI    , KC_LALT        , DF(_BASE)     , DF(_WORKMAN) , DF(_GAMEBASE) , DF(_FPS) , QK_BOOTLOADER , XXXXXXX , QK_BOOTLOADER
+            RGB_TOG       , RGB_MOD       , KC_LGUI    , KC_LALT        , DF(_BASE)     , DF(_COLEMAK_DHM) , DF(_GAMEBASE) , DF(_FPS) , QK_BOOTLOADER , XXXXXXX , QK_BOOTLOADER
             )             ,
 
     [_GAMEBASE] = LAYOUT(
